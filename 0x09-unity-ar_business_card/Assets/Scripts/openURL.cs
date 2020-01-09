6b82b0aabdde27b8c7a6142e75bf8a131bdfa780
c-eng <heirvuyar@gmail.com>
2020-01-08 23:22:50 -0800

    New: Adding AR Businesscard task

---
+++ 0x09-unity-ar_business_card/Assets/Scripts/openURL.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class openURL : MonoBehaviour
+{
+    public void TWurl()
+    {
+        Application.OpenURL("https://twitter.com/c33Eng");
+    }
+
+    public void GHurl()
+    {
+        Application.OpenURL("https://github.com/c-eng");
+    }
+
+    public void INurl()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/cameron-eng-5a8b7773/");
+    }
+
+    public void EMurl()
+    {
+        Application.OpenURL("mailto:388@holbertonschool.com");
+    }
+}
---
+++ 0x09-unity-ar_business_card/README.md
@@ -0,0 +1,12 @@
+# 0x09. Unity - AR Business Card
+### Description
+AR business card utilizing Vuforia and Unity.
+
+## Directory Contents
+
+|   ***File/Folder***    |  **Description**                       |
+|---------------|---------------------------------------|
+| `README.md` |  Read me file. |
+
+### Authors
+*Cameron Eng* - [Github](https://github.com/c-eng) || [Twitter](https://twitter.com/c33Eng)
